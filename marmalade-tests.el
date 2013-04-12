3e221b73139c591c173c325de13646a120962ed0
Nic Ferier <nic@ferrier.me.uk>
2013-04-13 00:09:30 +0100

    basics of marmalade

---
+++ marmalade-tests.el
@@ -0,0 +1,24 @@
+;;; tests for marmalade
+
+(require 'ert)
+(require 'marmalade-s)
+
+(ert-deftest marmalade-package-explode ()
+  "Make sure the regex works."
+  (let ((file "ascii-3.1.el"))
+    (destructuring-bind (name version type)
+        (marmalade/explode-package-string file)
+      (should (equal name "ascii"))
+      (should (equal version "3.1"))
+      (should (equal type "el")))))
+
+(ert-deftest marmalade-package-name->filename ()
+  "Convert package name to a filename."
+  (let ((file "ascii-3.1.el")
+        (marmalade-package-store-dir "/packages"))
+    (should
+     (equal
+      (marmalade/package-name->filename file)
+      "ascii/3.1/ascii-3.1.el"))))
+
+;;; marmalade-tests.el ends here
