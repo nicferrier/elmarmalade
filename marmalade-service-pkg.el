4673da620301db651fb0e34e591cdb03940495bd
Nic Ferier <nic@ferrier.me.uk>
2013-04-15 01:33:39 +0100

    melpa branch created by elpakit

--- marmalade-service-pkg.el
+++ marmalade-service-pkg.el
@@ -1,1 +1,1 @@
-(define-package "marmalade-service" "2.0.6" "The Marmalade package store service." (quote ((dash "1.0.3") (elnode "0.9.9.6.8"))))
+(define-package "marmalade-service" "2.0.7" "The Marmalade package store service." (quote ((dash "1.0.3") (elnode "0.9.9.6.8"))))
