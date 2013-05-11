7828a10e6278dab5cad0769e2a7390f71621cd73
Nic Ferier <nic@ferrier.me.uk>
2013-05-11 11:56:20 +0100

    add dash and outline to the requires

--- marmalade-service.el
+++ marmalade-service.el
@@ -24,7 +24,7 @@
 
 ;;; Code:
 
-(elnode-app marmalade-dir marmalade-archive db s rx lisp-mnt)
+(elnode-app marmalade-dir marmalade-archive db s dash rx lisp-mnt outline)
 
 (defconst marmalade-cookie-name "marmalade-user"
   "The name of the cookie we use for auth.")
