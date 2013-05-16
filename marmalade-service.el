4aa5dac506e1f67d6a4f2e818c8c03f0001b8743
Nic Ferier <nic@ferrier.me.uk>
2013-05-16 15:54:38 +0100

    docstring on help-panel

--- marmalade-service.el
+++ marmalade-service.el
@@ -336,7 +336,8 @@ is grabbed."
   "Template for package items.")
 
 (defconst marmalade/login-panel
-  "<div id=\"login-panel\">logged in: <span id=\"username\">${username}</span></div>")
+  "<div id=\"login-panel\">logged in: <span id=\"username\">${username}</span></div>"
+  "Login panel template.")
 
 (defun marmalade/login (httpcon)
   (let* ((auth-cookie-cons (elnode-auth-get-cookie-value
