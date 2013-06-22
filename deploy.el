07401e7384268182cec4e13e4cd741388ae5e7da
Nic Ferier <nic@ferrier.me.uk>
2013-06-22 12:56:15 +0100

    latest deploy script

--- deploy.el
+++ deploy.el
@@ -8,9 +8,11 @@
    ("marmalade" . "http://marmalade-repo.org/packages/")))
 (customize-set-variable 'elnode-init nil)
 (customize-set-variable 'elnode-log-files-directory (expand-file-name "logs" deploy-dir))
+(customize-set-variable 'lisp-indent-function 'common-lisp-indent-function)
 (customize-set-variable 'marmalade-server-port 8005)
 (customize-set-variable 'marmalade-db-dir (expand-file-name "~/marmalade/db"))
 (customize-set-variable 'marmalade-package-store-dir (expand-file-name "~/marmalade/packages"))
+(customize-set-variable 'marmalade-archive-dir (expand-file-name "~/marmalade/archives"))
 (customize-set-variable 'marmalade-archive-port 8006)
 (customize-set-variable 'marmalade-boot-onload t)
 (customize-set-variable 'debug-on-error 'always)
@@ -20,7 +22,7 @@
 (package-refresh-contents)
 (package-install-file
  (expand-file-name
-  "marmalade-service-2.0.9.tar"
+  "marmalade-service-2.0.10.tar"
   default-directory))
 (with-temp-buffer
   (print
