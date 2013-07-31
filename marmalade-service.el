7b578d46798f28342a9c0e447001ee6a3420b65b
Nic Ferrier <nferrier@ferrier.me.uk>
2013-07-31 05:41:50 +0100

    bunch of bootstrap updates

--- marmalade-service.el
+++ marmalade-service.el
@@ -62,6 +62,23 @@ file."
                          "user")))
   "The user database.")
 
+(defconst marmalade/page-header "<div class=\"navbar\">
+            <div class=\"navbar-inner\">
+                <ul class=\"nav pull-right\">
+                    <li class=\"active\"><a href=\"/\">marmalade-repo</a></li>
+                    <li><a href=\"#\">login</a></li>
+                    <li><a href=\"#\">register</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id=\"github\">
+            <a href=\"https://github.com/nicferrier/elmarmalade\">
+                <img style=\"position: absolute; top: 0; left: 0; border: 0;\" 
+                     src=\"https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png\" 
+                     alt=\"Fork me on GitHub\"></img>
+            </a>
+        </div>")
+
 (defun marmalade/explode-package-string (package-name)
   (string-match
    "\\(.+\\)-\\([0-9.]+\\)+\\.\\(el\\|tar\\)"
@@ -343,23 +360,6 @@ is grabbed."
           (insert commentary)
           (marmalade/commentary-grab (current-buffer))))
       ""))
-
-(defconst marmalade/page-header "<div class=\"navbar\">
-            <div class=\"navbar-inner\">
-                <ul class=\"nav pull-right\">
-                    <li class=\"active\"><a href=\"/\">marmalade-repo</a></li>
-                    <li><a href=\"#\">login</a></li>
-                    <li><a href=\"#\">register</a></li>
-                </ul>
-            </div>
-        </div>
-        <div id=\"github\">
-            <a href=\"https://github.com/nicferrier/elmarmalade\">
-                <img style=\"position: absolute; top: 0; left: 0; border: 0;\" 
-                     src=\"https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png\" 
-                     alt=\"Fork me on GitHub\"></img>
-            </a>
-        </div>")
 
 (defun marmalade/package-blurb (httpcon)
   "Provide an informative description of the package."
