1538126e420bd5b8a9dfce51f10eb7daeb5f4297
Nic Ferrier <nferrier@ferrier.me.uk>
2014-06-02 23:59:39 +0100

    add new marmalade-vars and move the page-header vars to it

--- marmalade-service.el
+++ marmalade-service.el
@@ -25,49 +25,13 @@
 ;;; Code:
 
 (elnode-app marmalade-dir
-  marmalade-customs marmalade-archive marmalade-users
+  marmalade-vars marmalade-customs marmalade-archive marmalade-users
   file-format ; should be a separate package
   web htmlize db s
   noflet dash kv rx lisp-mnt outline)
 
 (defconst marmalade/cookie-name "marmalade-user"
   "The name of the cookie we use for auth.")
-
-(defconst marmalade/page-header "<div class=\"navbar\">
-            <div class=\"navbar-inner\">
-                <ul class=\"nav pull-right\">
-                    <li class=\"active\"><a href=\"/\">marmalade-repo</a></li>
-                    <li><a href=\"/login/\">login</a></li>
-                    <li><a href=\"/register-comingsoon/\">register</a></li>
-                </ul>
-            </div>
-        </div>
-        <div id=\"github\">
-            <a href=\"https://github.com/nicferrier/elmarmalade\">
-                <img style=\"position: absolute; top: 0; left: 0; border: 0;\" 
-                     src=\"https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png\" 
-                     alt=\"Fork me on GitHub\"></img>
-            </a>
-        </div>"
-  "The page header for logged out users.")
-
-(defconst marmalade/page-header-loggedin "<div class=\"navbar\">
-            <div class=\"navbar-inner\">
-                <ul class=\"nav pull-right\">
-                    <li class=\"active\"><a href=\"/\">marmalade-repo</a></li>
-                    <li><a href=\"/profile/${username}/\">${username}</a></li>
-                </ul>
-            </div>
-        </div>
-        <div id=\"github\">
-            <a href=\"https://github.com/nicferrier/elmarmalade\">
-                <img style=\"position: absolute; top: 0; left: 0; border: 0;\" 
-                     src=\"https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png\" 
-                     alt=\"Fork me on GitHub\"></img>
-            </a>
-        </div>"
-  "The page header for logged-in users.")
-
 
 (defun marmalade/login-sender (httpcon _target _redirect)
   "Send the login page."
