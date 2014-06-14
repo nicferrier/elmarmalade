09cfcd16d8c86f401ae681b26123b93504c6394b
Nic Ferrier <nferrier@ferrier.me.uk>
2014-06-14 13:03:43 +0100

    update the version on the demo package so we can test upload

--- marmalade-demo.el
+++ marmalade-demo.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: lisp
-;; Version: 0.0.3
+;; Version: 0.0.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
